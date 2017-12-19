f904f5ae81701dcf64889de660340c9188284494
Michele Segata <segata@ccs-labs.org>
2017-12-19 13:43:08 +0100

    add run_terminal_command upi

--- wishful_upis/mgmt.py
+++ wishful_upis/mgmt.py
@@ -70,3 +70,9 @@ def get_hostname():
     """ Returns the hostname for the node
     """
     return
+
+
+def run_terminal_command(cmd, cwd=None):
+    """Runs a command in the terminal
+    """
+    return
